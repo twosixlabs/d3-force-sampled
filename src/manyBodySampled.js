8bc10c412f2e7f0bdf93741afb0b2d18c792440d
Robert Gove <rpgove@gmail.com>
2018-03-31 18:43:58 -0400

    Set update size, sample size, RNG, charge multiplier

--- src/manyBodySampled.js
+++ src/manyBodySampled.js
@@ -5,17 +5,26 @@ export default function() {
       alpha,
       strength = constant(-30),
       strengths,
-      sqrt_n,
-      indicesForce,
       indicesRepulse,
       prevIndex = 0,
       distanceMin2 = 1,
       distanceMax2 = Infinity,
-      numNN = constant(0),
-      maxNN;
+      neighborSize = function () {
+        return 15;
+      },
+      updateSize = function (nodes) { return Math.pow(nodes.length, 0.75); },
+      sampleSize = function (nodes) { return Math.pow(nodes.length, 0.25); },
+      numNeighbors,
+      numUpdate,
+      numSamples,
+      chargeMultiplier = function (nodes) {
+        return nodes.length < 100 ? 1 : nodes.length < 200 ? 3 : Math.sqrt(nodes.length);
+      },
+      cMult,
+      rand = Math.random;
 
   function addRandomNode(node) {
-    var randIdx = Math.floor(Math.random() * nodes.length),
+    var randIdx = Math.floor(rand() * nodes.length),
         randNode = nodes[randIdx],
         randDist = (node.x - randNode.x) * (node.x - randNode.x) + (node.y - randNode.y) * (node.y - randNode.y),
         currIdx,
@@ -25,20 +34,20 @@ export default function() {
         maxDist = -Infinity,
         i = -1;
 
+    // Is this already in the list?
+    if (node.nearest.indexOf(randIdx) >= 0) return;
+
     // If there is room for another, add it.
-    if (node.nearest.length < maxNN) {
+    if (node.nearest.length < numNeighbors) {
       node.nearest.push(randIdx);
       return;
     }
-
-    // Is this already in the list?
-    if (node.nearest.indexOf(randIdx) >= 0) return;
 
     // Replace the farthest away "neighbor" with the new node.
     while (++i < node.nearest.length) {
       currIdx = node.nearest[i];
       currNode = nodes[currIdx];
-      currDist = (node.x - currNode.x) * (node.x - currNode.x) + (node.y - currNode.y) * (node.y - currNode.y);
+      currDist = Math.hypot(node.x - currNode.x, node.y - currNode.y);
       if (currDist > maxDist) {
         maxI = i;
         maxDist = currDist;
@@ -60,7 +69,7 @@ export default function() {
 
     // Choose random indices.
     for (i = n-1; i >= cnt; --i) {
-      randIdx = Math.floor(Math.random() * i);
+      randIdx = Math.floor(rand() * i);
       temp = indices[randIdx];
       indices[randIdx] = indices[i];
       indices[i] = temp;
@@ -72,18 +81,14 @@ export default function() {
   function approxRepulse(node) {
     var i,
         randIndices,
-        nearest,
         currNode,
         w,
         x,
         y,
         l;
 
-    // Update the list of nearest nodes.
-    if (maxNN) addRandomNode(node);
-
-    // Choose random sqrt(n) nodes.
-    randIndices = getRandIndices(indicesRepulse, sqrt_n);
+    // Choose random nodes to update.
+    randIndices = getRandIndices(indicesRepulse, numSamples);
 
     for (i = randIndices.length - 1; i >= 0; --i) {
       currNode = nodes[randIndices[i]];
@@ -97,34 +102,11 @@ export default function() {
       if (l >= distanceMax2) continue;
 
       // Limit forces for very close nodes; randomize direction if coincident.
-      if (x === 0) x = (Math.random() - 0.5) * 1e-6, l += x * x;
-      if (y === 0) y = (Math.random() - 0.5) * 1e-6, l += y * y;
+      if (x === 0) x = (rand() - 0.5) * 1e-6, l += x * x;
+      if (y === 0) y = (rand() - 0.5) * 1e-6, l += y * y;
       if (l < distanceMin2) l = Math.sqrt(distanceMin2 * l);
 
-      w = strengths[node.index] * alpha * Math.pow(2,-alpha) * sqrt_n / l;
-      node.vx += x * w;
-      node.vy += y * w;
-    }
-
-    nearest = node.nearest;
-
-    if (maxNN) for (i = nearest.length - 1; i >= 0; --i) {
-      currNode = nodes[nearest[i]];
-
-      if (currNode === node) continue;
-
-      x = currNode.x - node.x;
-      y = currNode.y - node.y;
-      l = x * x + y * y;
-
-      if (l >= distanceMax2) continue;
-
-      // Limit forces for very close nodes; randomize direction if coincident.
-      if (x === 0) x = (Math.random() - 0.5) * 1e-6, l += x * x;
-      if (y === 0) y = (Math.random() - 0.5) * 1e-6, l += y * y;
-      if (l < distanceMin2) l = Math.sqrt(distanceMin2 * l);
-
-      w = strengths[node.index] * alpha * Math.pow(2,-alpha) * sqrt_n / l;
+      w = strengths[node.index] * alpha * cMult / l;
       node.vx += x * w;
       node.vy += y * w;
     }
@@ -140,11 +122,11 @@ export default function() {
         l;
 
     // Update the list of nearest nodes.
-    if (maxNN) addRandomNode(node);
+    if (numNeighbors) addRandomNode(node);
 
     nearest = node.nearest;
 
-    if (maxNN) for (i = nearest.length - 1; i >= 0; --i) {
+    if (numNeighbors) for (i = nearest.length - 1; i >= 0; --i) {
       currNode = nodes[nearest[i]];
 
       if (currNode === node) continue;
@@ -156,42 +138,48 @@ export default function() {
       if (l >= distanceMax2) continue;
 
       // Limit forces for very close nodes; randomize direction if coincident.
-      if (x === 0) x = (Math.random() - 0.5) * 1e-6, l += x * x;
-      if (y === 0) y = (Math.random() - 0.5) * 1e-6, l += y * y;
+      if (x === 0) x = (rand() - 0.5) * 1e-6, l += x * x;
+      if (y === 0) y = (rand() - 0.5) * 1e-6, l += y * y;
       if (l < distanceMin2) l = Math.sqrt(distanceMin2 * l);
 
-      w = strengths[node.index] * alpha * Math.pow(10,-alpha) * sqrt_n / l;
+      w = strengths[node.index] * alpha * cMult / l;
       node.vx += x * w;
       node.vy += y * w;
     }
   }
 
   function force(_) {
-    var i = 0, n = nodes.length, upperIndex = (prevIndex + Math.floor(sqrt_n)) % n;
-    for (alpha = _; i < n; ++i) {
-      if ((prevIndex < upperIndex && i >= prevIndex && i < upperIndex) ||
-        (prevIndex > upperIndex && (i >= prevIndex || i < upperIndex))) approxRepulse(nodes[i]);
-      else if (maxNN) constantRepulse(nodes[i]);
+    var i = 0, j = prevIndex, n = nodes.length, upperIndex = prevIndex + numUpdate;
+    for (alpha = _; i < n || j < upperIndex; ++i, ++j) {
+      if (j < upperIndex) approxRepulse(nodes[j%n]);
+      if (numNeighbors && i < n) constantRepulse(nodes[i]);
     }
-    prevIndex = upperIndex;
+    prevIndex = upperIndex % n;
   }
 
   function initialize() {
     if (!nodes) return;
     var i, n = nodes.length, node;
-    sqrt_n = Math.sqrt(n);
-    indicesForce = new Array(n);
-    for (i = 0; i < n; ++i) indicesForce[i] = i;
-    indicesRepulse = indicesForce.slice();
+    indicesRepulse = new Array(n);
+    for (i = 0; i < n; ++i) indicesRepulse[i] = i;
     strengths = new Array(n);
     
-    // Number of nearest neighbors cannot be negative.
-    maxNN = Math.floor(numNN(nodes));
-    maxNN = maxNN < 0 ? 0 : Math.min(maxNN, nodes.length);
+    // Cannot be negative.
+    numNeighbors = Math.min(Math.ceil(neighborSize(nodes)), n);
+    numNeighbors = numNeighbors < 0 ? 0 : Math.min(numNeighbors, nodes.length);
+    numUpdate = Math.ceil(updateSize(nodes));
+    numUpdate = numUpdate < 0 ? 0 : Math.min(numUpdate, n);
+    numSamples = Math.ceil(sampleSize(nodes));
+    numSamples = numSamples < 0 ? 0 : Math.min(numSamples, n);
+
+    cMult = chargeMultiplier(nodes);
+
+    alpha = 1;
     for (i = 0; i < n; ++i) {
       node = nodes[i];
       strengths[node.index] = +strength(node, i, nodes);
       node.nearest = [];
+      while (node.nearest.length < numNeighbors) addRandomNode(node);
     }
   }
 
@@ -212,8 +200,24 @@ export default function() {
     return arguments.length ? (distanceMax2 = _ * _, force) : Math.sqrt(distanceMax2);
   };
 
-  force.useNearestNeighbors = function(_) {
-    return arguments.length ? (numNN = typeof _ === "function" ? _ : constant(+_), initialize(), force) : numNN;
+  force.neighborSize = function(_) {
+    return arguments.length ? (neighborSize = typeof _ === "function" ? _ : constant(+_), initialize(), force) : neighborSize;
+  };
+
+  force.updateSize = function(_) {
+    return arguments.length ? (updateSize = typeof _ === "function" ? _ : constant(+_), initialize(), force) : updateSize;
+  };
+
+  force.sampleSize = function(_) {
+    return arguments.length ? (sampleSize = typeof _ === "function" ? _ : constant(+_), initialize(), force) : sampleSize;
+  };
+
+  force.chargeMultiplier = function(_) {
+    return arguments.length ? (chargeMultiplier = typeof _ === "function" ? _ : constant(+_), initialize(), force) : chargeMultiplier;
+  };
+
+  force.source = function(_) {
+    return arguments.length ? (rand = _, force) : rand;
   };
 
   return force;
